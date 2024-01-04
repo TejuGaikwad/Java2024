3f9af6c7358414d1ff8e45d53c9429c5e5b299e6
TejuGaikwad <masterofscience050@gmail.com>
2024-01-04 20:06:00 +0530

    New Files added

--- src/First24.java
+++ src/First24.java
@@ -1,2 +1,13 @@
 public class First24 {
+    public static void main(String[] args){
+        System.out.println("2024 pehla code shuru!");
+    }
+}
+
+class Tejas {
+    int a;
+    int b;
+    Tejas(int a, int b){
+
+    }
 }
---
+++ src/Second24.java
@@ -0,0 +1,14 @@
+public class Second24 {
+    public static void main(String[] args){
+        System.out.println("");
+        int a = 12;
+        float b = 4.9f;
+        long s = 48765876587658765L;
+        double d = 5.231342344534534434d;
+        byte t = 107;
+        if(t < Byte.MAX_VALUE){
+            System.out.println("Max value is not reached");
+        }
+
+    }
+}
